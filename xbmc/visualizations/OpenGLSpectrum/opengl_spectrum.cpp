6cde899a52399dd4584509cd05321ecb568901bd
Tobias Arrskog <topfs2@xbmc.org>
2014-10-08 08:17:49 +0200

    Initial testing of spectrum and fft

--- xbmc/visualizations/OpenGLSpectrum/opengl_spectrum.cpp
+++ xbmc/visualizations/OpenGLSpectrum/opengl_spectrum.cpp
@@ -35,74 +35,45 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
+#include <complex.h>
 #include <stdint.h>
-
-#if defined(HAS_GLES)
-#include "VisGUIShader.h"
-
-#ifndef M_PI
-#define M_PI       3.141592654f
-#endif
-#define DEG2RAD(d) ( (d) * M_PI/180.0f )
-
-//OpenGL wrapper - allows us to use same code of functions draw_bars and render
-#define GL_PROJECTION             MM_PROJECTION
-#define GL_MODELVIEW              MM_MODELVIEW
-
-#define glPushMatrix()            vis_shader->PushMatrix()
-#define glPopMatrix()             vis_shader->PopMatrix()
-#define glTranslatef(x,y,z)       vis_shader->Translatef(x,y,z)
-#define glRotatef(a,x,y,z)        vis_shader->Rotatef(DEG2RAD(a),x,y,z)
-#define glPolygonMode(a,b)        ;
-#define glBegin(a)                vis_shader->Enable()
-#define glEnd()                   vis_shader->Disable()
-#define glMatrixMode(a)           vis_shader->MatrixMode(a)
-#define glLoadIdentity()          vis_shader->LoadIdentity()
-#define glFrustum(a,b,c,d,e,f)    vis_shader->Frustum(a,b,c,d,e,f)
-
-static GLenum  g_mode = GL_TRIANGLES;
-static const char *frag = "precision mediump float; \n"
-                   "varying lowp vec4 m_colour; \n"
-                   "void main () \n"
-                   "{ \n"
-                   "  gl_FragColor = m_colour; \n"
-                   "}\n";
-
-static const char *vert = "attribute vec4 m_attrpos;\n"
-                   "attribute vec4 m_attrcol;\n"
-                   "attribute vec4 m_attrcord0;\n"
-                   "attribute vec4 m_attrcord1;\n"
-                   "varying vec4   m_cord0;\n"
-                   "varying vec4   m_cord1;\n"
-                   "varying lowp   vec4 m_colour;\n"
-                   "uniform mat4   m_proj;\n"
-                   "uniform mat4   m_model;\n"
-                   "void main ()\n"
-                   "{\n"
-                   "  mat4 mvp    = m_proj * m_model;\n"
-                   "  gl_Position = mvp * m_attrpos;\n"
-                   "  m_colour    = m_attrcol;\n"
-                   "  m_cord0     = m_attrcord0;\n"
-                   "  m_cord1     = m_attrcord1;\n"
-                   "}\n";
-
-static CVisGUIShader *vis_shader = NULL;
-
-#elif defined(HAS_SDL_OPENGL)
+#include <iostream>
 #include <GL/glew.h>
+
 static GLenum  g_mode = GL_FILL;
 
-#endif
+double PI = atan2(1, 1) * 4;
+typedef double complex cplx;
+ 
+void _fft(cplx buf[], cplx out[], int n, int step)
+{
+	if (step < n) {
+		_fft(out, buf, n, step * 2);
+		_fft(out + step, buf + step, n, step * 2);
+ 
+		for (int i = 0; i < n; i += 2 * step) {
+			cplx t = cexp(-I * PI * i / n) * out[i + step];
+			buf[i / 2]     = out[i] + t;
+			buf[(i + n)/2] = out[i] - t;
+		}
+	}
+}
+ 
+void fft(cplx buf[], int n)
+{
+	cplx out[n];
+	for (int i = 0; i < n; i++) out[i] = buf[i];
+ 
+	_fft(buf, out, n, 1);
+}
 
-#define NUM_BANDS 16
+#define AUDIO_BUFFER 512
+#define NUM_BANDS (AUDIO_BUFFER / 2)
 
-static GLfloat x_angle = 20.0, x_speed = 0.0;
-static GLfloat y_angle = 45.0, y_speed = 0.5;
-static GLfloat z_angle = 0.0, z_speed = 0.0;
-static GLfloat heights[16][16], cHeights[16][16], scale;
-static GLfloat hSpeed = 0.05;
+static GLfloat heights[NUM_BANDS];
+static GLfloat scale;
+static float hSpeed;
 
-#if defined(HAS_SDL_OPENGL)
 static void draw_rectangle(GLfloat x1, GLfloat y1, GLfloat z1, GLfloat x2, GLfloat y2, GLfloat z2)
 {
   if(y1 == y2)
@@ -127,172 +98,24 @@ static void draw_rectangle(GLfloat x1, GLfloat y1, GLfloat z1, GLfloat x2, GLflo
   }
 }
 
-static void draw_bar(GLfloat x_offset, GLfloat z_offset, GLfloat height, GLfloat red, GLfloat green, GLfloat blue, int index)
-{
-  GLfloat width = 0.1;
-
-  if (g_mode == GL_POINT)
-    glColor3f(0.2, 1.0, 0.2);
-
-  if (g_mode != GL_POINT)
-  {
-    glColor3f(red,green,blue);
-    draw_rectangle(x_offset, height, z_offset, x_offset + width, height, z_offset + 0.1);
-  }
-  draw_rectangle(x_offset, 0, z_offset, x_offset + width, 0, z_offset + 0.1);
-
-  if (g_mode != GL_POINT)
-  {
-    glColor3f(0.5 * red, 0.5 * green, 0.5 * blue);
-    draw_rectangle(x_offset, 0.0, z_offset + 0.1, x_offset + width, height, z_offset + 0.1);
-  }
-  draw_rectangle(x_offset, 0.0, z_offset, x_offset + width, height, z_offset );
-
-  if (g_mode != GL_POINT)
-  {
-    glColor3f(0.25 * red, 0.25 * green, 0.25 * blue);
-    draw_rectangle(x_offset, 0.0, z_offset , x_offset, height, z_offset + 0.1);
-  }
-  draw_rectangle(x_offset + width, 0.0, z_offset , x_offset + width, height, z_offset + 0.1);
-}
-
-#elif defined(HAS_GLES)
-
-static GLfloat  *m_col;
-static GLfloat  *m_ver;
-static GLushort *m_idx;
-
-static void draw_bar(GLfloat x_offset, GLfloat z_offset, GLfloat height, GLfloat red, GLfloat green, GLfloat blue, int index)
-{
-  if (!m_col || !m_ver || !m_idx)
-    return;
-
-  // avoid zero sized bars, which results in overlapping triangles of same depth and display artefacts
-  height = std::max(height, 1e-3f);
-  GLfloat *ver = m_ver + 3 * 8 * index;
-  // just need to update the height vertex, all else is the same
-  for (int i=0; i<8; i++)
-  {
-    ver[1] = (((i+0)>>2)&1) * height;
-    ver += 3;
-  }
-  // on last index, draw the object
-  if (index == 16*16-1)
-  {
-    GLint   posLoc = vis_shader->GetPosLoc();
-    GLint   colLoc = vis_shader->GetColLoc();
-
-    glVertexAttribPointer(colLoc, 3, GL_FLOAT, 0, 0, m_col);
-    glVertexAttribPointer(posLoc, 3, GL_FLOAT, 0, 0, m_ver);
-
-    glEnableVertexAttribArray(posLoc);
-    glEnableVertexAttribArray(colLoc);
-
-    glDrawElements(g_mode, 16*16*36, GL_UNSIGNED_SHORT, m_idx);
-
-    glDisableVertexAttribArray(posLoc);
-    glDisableVertexAttribArray(colLoc);
-  }
-}
-
-static void init_bars(void)
-{
-  if (!m_col || !m_ver || !m_idx)
-    return;
-
-  GLfloat x_offset, z_offset, r_base, b_base;
-  for(int y = 0; y < 16; y++)
-  {
-    z_offset = -1.6 + ((15 - y) * 0.2);
-
-    b_base = y * (1.0 / 15);
-    r_base = 1.0 - b_base;
-
-    for(int x = 0; x < 16; x++)
-    {
-      x_offset = -1.6 + ((float)x * 0.2);
-
-      GLfloat red = r_base - (float(x) * (r_base / 15.0));
-      GLfloat green = (float)x * (1.0 / 15);
-      GLfloat blue = b_base;
-      int index = 16*y+x;
-      GLfloat *col = m_col + 3 * 8 * index;
-      for (int i=0; i<8; i++)
-      {
-        float scale = 0.1f * i;
-        *col++ = red   * scale;
-        *col++ = green * scale;
-        *col++ = blue  * scale;
-      }
-      GLfloat *ver = m_ver + 3 * 8 * index;
-      for (int i=0; i<8; i++)
-      {
-        *ver++ = x_offset + (((i+1)>>1)&1) * 0.1f;
-        *ver++ = 0; // height - filled in later
-        *ver++ = z_offset + (((i+0)>>1)&1) * 0.1f;
-      }
-      GLushort *idx = m_idx + 36 * index;
-      GLushort startidx = 8 * index;
-      // Bottom
-      *idx++ = startidx + 0; *idx++ = startidx + 1; *idx++ = startidx + 2;
-      *idx++ = startidx + 0; *idx++ = startidx + 2; *idx++ = startidx + 3;
-      // Left
-      *idx++ = startidx + 0; *idx++ = startidx + 4; *idx++ = startidx + 7;
-      *idx++ = startidx + 0; *idx++ = startidx + 7; *idx++ = startidx + 3;
-      // Back
-      *idx++ = startidx + 3; *idx++ = startidx + 7; *idx++ = startidx + 6;
-      *idx++ = startidx + 3; *idx++ = startidx + 6; *idx++ = startidx + 2;
-      // Right
-      *idx++ = startidx + 1; *idx++ = startidx + 5; *idx++ = startidx + 6;
-      *idx++ = startidx + 1; *idx++ = startidx + 6; *idx++ = startidx + 2;
-      // Front
-      *idx++ = startidx + 0; *idx++ = startidx + 4; *idx++ = startidx + 5;
-      *idx++ = startidx + 0; *idx++ = startidx + 5; *idx++ = startidx + 1;
-      // Top
-      *idx++ = startidx + 4; *idx++ = startidx + 5; *idx++ = startidx + 6;
-      *idx++ = startidx + 4; *idx++ = startidx + 6; *idx++ = startidx + 7;
-    }
-  }
-}
-#endif
-
 static void draw_bars(void)
 {
-  int x,y;
-  GLfloat x_offset, z_offset, r_base, b_base;
-
   glClear(GL_DEPTH_BUFFER_BIT);
   glPushMatrix();
-  glTranslatef(0.0,-0.5,-5.0);
-  glRotatef(x_angle,1.0,0.0,0.0);
-  glRotatef(y_angle,0.0,1.0,0.0);
-  glRotatef(z_angle,0.0,0.0,1.0);
   
   glPolygonMode(GL_FRONT_AND_BACK, g_mode);
   glBegin(GL_TRIANGLES);
-  
-  for(y = 0; y < 16; y++)
-  {
-    z_offset = -1.6 + ((15 - y) * 0.2);
 
-    b_base = y * (1.0 / 15);
-    r_base = 1.0 - b_base;
+  GLfloat padding = 0.1;
+  GLfloat xscale = (2.0 - padding * 2.0) / (NUM_BANDS * 1.0);
+  GLfloat x = -1.0f + padding;
 
-    for(x = 0; x < 16; x++)
-    {
-      x_offset = -1.6 + ((float)x * 0.2);
-      if (::fabs(cHeights[y][x]-heights[y][x])>hSpeed)
-      {
-        if (cHeights[y][x]<heights[y][x])
-          cHeights[y][x] += hSpeed;
-        else
-          cHeights[y][x] -= hSpeed;
-      }
-      draw_bar(x_offset, z_offset,
-        cHeights[y][x], r_base - (float(x) * (r_base / 15.0)),
-        (float)x * (1.0 / 15), b_base, 16*y+x);
-    }
+  glColor3f(0.2, 1.0, 0.2);
+  for (unsigned int i = 0; i < NUM_BANDS; i++) {
+    draw_rectangle(x, -0.9f, 0.0f, x + xscale * 0.5, heights[i] - 0.85f, 0.0f);
+    x += xscale;
   }
+
   glEnd();
   glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
   glPopMatrix();
@@ -303,29 +126,12 @@ static void draw_bars(void)
 //-----------------------------------------------------------------------------
 ADDON_STATUS ADDON_Create(void* hdl, void* props)
 {
+
   if (!props)
     return ADDON_STATUS_UNKNOWN;
 
   scale = 1.0 / log(256.0);
-
-#if defined(HAS_GLES)
-  vis_shader = new CVisGUIShader(vert, frag);
-
-  if(!vis_shader)
-    return ADDON_STATUS_UNKNOWN;
-
-  if(!vis_shader->CompileAndLink())
-  {
-    delete vis_shader;
-    return ADDON_STATUS_UNKNOWN;
-  }  
-  m_col = (GLfloat  *)malloc(16*16*3*8 * sizeof(GLfloat *));
-  m_ver = (GLfloat  *)malloc(16*16*3*8 * sizeof(GLfloat *));
-  m_idx = (GLushort *)malloc(16*16*36  * sizeof(GLushort *));
-  init_bars();
-#endif
-
-  scale = 1.0 / log(256.0);
+  hSpeed = 0.0;
 
   return ADDON_STATUS_NEED_SETTINGS;
 }
@@ -339,27 +145,16 @@ extern "C" void Render()
   glMatrixMode(GL_PROJECTION);
   glPushMatrix();
   glLoadIdentity();
-  glFrustum(-1, 1, -1, 1, 1.5, 10);
+  glOrtho(-1, 1, -1, 1, -1, 1);
   glMatrixMode(GL_MODELVIEW);
   glPushMatrix();
   glLoadIdentity();
   glEnable(GL_DEPTH_TEST);
   glDepthFunc(GL_LESS);
   glPolygonMode(GL_FRONT, GL_FILL);
-  //glPolygonMode(GL_BACK, GL_FILL);
-  x_angle += x_speed;
-  if(x_angle >= 360.0)
-    x_angle -= 360.0;
-
-  y_angle += y_speed;
-  if(y_angle >= 360.0)
-    y_angle -= 360.0;
-
-  z_angle += z_speed;
-  if(z_angle >= 360.0)
-    z_angle -= 360.0;
 
   draw_bars();
+
   glPopMatrix();
   glMatrixMode(GL_PROJECTION);
   glPopMatrix();
@@ -369,58 +164,38 @@ extern "C" void Render()
 
 extern "C" void Start(int iChannels, int iSamplesPerSec, int iBitsPerSample, const char* szSongName)
 {
-  int x, y;
-
-  for(x = 0; x < 16; x++)
+  for(unsigned int x = 0; x < NUM_BANDS; x++)
   {
-    for(y = 0; y < 16; y++)
-    {
-      cHeights[y][x] = 0.0;
-    }
+    heights[x] = 0.0;
   }
-
-  x_speed = 0.0;
-  y_speed = 0.5;
-  z_speed = 0.0;
-  x_angle = 20.0;
-  y_angle = 45.0;
-  z_angle = 0.0;
 }
 
 extern "C" void AudioData(const float* pAudioData, int iAudioDataLength, float *pFreqData, int iFreqDataLength)
 {
-  int i,c;
-  int y=0;
-  GLfloat val;
+  cplx buf[iAudioDataLength];
 
-  int xscale[] = {0, 1, 2, 3, 5, 7, 10, 14, 20, 28, 40, 54, 74, 101, 137, 187, 255};
-
-  for(y = 15; y > 0; y--)
-  {
-    for(i = 0; i < 16; i++)
-    {
-      heights[y][i] = heights[y - 1][i];
-    }
+  for (unsigned int i = 0; i < iAudioDataLength; i++) {
+    buf[i] = pAudioData[i];
   }
 
-  for(i = 0; i < NUM_BANDS; i++)
+  fft(buf, iAudioDataLength);
+
+/*
+  float fMinData = (float)iAudioDataLength * (float)iAudioDataLength * 3.0f / 8.0f * 0.5 * 0.5; // 3/8 for the Hann window, 0.5 as minimum amplitude
+  float fInvMinData = 1.0f/fMinData;
+*/
+
+  float fInvMinData = 1.0f / (sqrt(iAudioDataLength) * 8.0);
+
+  //float fInvMinData = 1.0f / iAudioDataLength;
+
+  for(unsigned int i = 0; i < std::max(NUM_BANDS, iAudioDataLength); i++)
   {
-    for(c = xscale[i], y = 0; c < xscale[i + 1]; c++)
-    {
-      if (c<iAudioDataLength)
-      {
-        if((int)(pAudioData[c] * (INT16_MAX)) > y)
-          y = (int)(pAudioData[c] * (INT16_MAX));
-      }
-      else
-        continue;
+    if (i > iAudioDataLength) {
+      heights[i] = 0.0f;
+    } else {
+      heights[i] = cabs(buf[i]) * fInvMinData;
     }
-    y >>= 7;
-    if(y > 0)
-      val = (logf(y) * scale);
-    else
-      val = 0;
-    heights[0][i] = val;
   }
 }
 
@@ -490,19 +265,6 @@ extern "C" void ADDON_Stop()
 //-----------------------------------------------------------------------------
 extern "C" void ADDON_Destroy()
 {
-#if defined(HAS_GLES)
-  if(vis_shader) 
-  {
-    vis_shader->Free();
-    delete vis_shader;
-  }
-  free(m_col);
-  free(m_ver);
-  free(m_idx);
-  m_col = NULL;
-  m_ver = NULL;
-  m_idx = NULL;
-#endif
 }
 
 //-- HasSettings --------------------------------------------------------------
